9ff801990d74228c2bdf2224b1bb1a68b379f4df
Nikita Tikhonov <nekit@nekit.dev>
2026-05-28 19:10:21 +0300

    Add `hello` example.

---
+++ src/hello.cpp
@@ -0,0 +1,5 @@
+#include "hello.h"
+
+void printHelloWorld(std::ostream& stream) {
+    stream << "Hello, world!\n";
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "hello.h"
+
+int main() {
+	  printHelloWorld(std::cout);
+
+	  return 0;
+}
